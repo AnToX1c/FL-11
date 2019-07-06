3d73b9b83a21ab59ac5f57264b4bb9138b01d353
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-07-06 18:20:14 +0300

    :six: Done Intro to JS HW

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,9 @@
-// Your code goes here
+const TWO = 2;
+const a1 = parseFloat(prompt("Please, enter the A point's numeric value (X)"));
+const a2 = parseFloat(prompt("Please, enter the A point's numeric value (Y)"));
+const b1 = parseFloat(prompt("Please, enter the B point's numeric value (X)"));
+const b2 = parseFloat(prompt("Please, enter the B point's numeric value (Y)"));
+const c1 = parseFloat(prompt("Please, enter the C point's numeric value (X)"));
+const c2 = parseFloat(prompt("Please, enter the C point's numeric value (Y)"));
+
+console.log(c1 === (a1 + b1) / TWO && c2 === (a2 + b2) / TWO);
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,13 @@
-// Your code goes here
+const sideAB = parseFloat(prompt('Please, enter the length of the side AB:'));
+const sideBC = parseFloat(prompt('Please, enter the length of the side BC:'));
+const sideCA = parseFloat(prompt('Please, enter the length of the side CA:'));
+
+if (!(sideAB+sideBC>sideCA && sideBC+sideCA>sideAB && sideCA+sideAB>sideBC)) {
+  console.log('Triangle doesn’t exist');
+} else if (sideAB === sideBC && sideAB === sideCA) {
+  console.log('Equivalent triangle');
+} else if (sideAB === sideBC || sideAB === sideCA || sideBC === sideCA) {
+  console.log('Isosceles triangle');
+} else {
+  console.log('Normal triangle');
+}

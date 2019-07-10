4c5c030be69e9f04cbee83cce45ee13014a445e7
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-07-10 07:04:35 +0300

    :seven: Done Control flow HW

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,1 +1,48 @@
-// Your code goes here
+const EMAIL_MIN_LENGTH = 6;
+const PASSWORD_MIN_LENGTH = 5;
+const email = prompt(`Please, enter your email:`, ``);
+
+if (email === `` || email === null) {
+  alert(`Canceled.`);
+} else if (email.length < EMAIL_MIN_LENGTH) {
+  alert(`I don’t know any emails having name length less than ${EMAIL_MIN_LENGTH} symbols`);
+} else if (email === `user@gmail.com` || email === `admin@gmail.com`) {
+  let currentPassword = prompt(`Please, enter your password:`, ``);
+  if (currentPassword === `` || currentPassword === null) {
+    alert(`Canceled.`);
+  } else if (currentPassword === `UserPass` && email === `user@gmail.com`
+        || currentPassword === `AdminPass` && email === `admin@gmail.com`) {
+    let isPassShouldChange = confirm(`Do you want to change your password?`);
+    if (isPassShouldChange) {
+      let confirmedPassword = prompt(`Please, enter your old password:`, ``);
+      if (confirmedPassword === `` || confirmedPassword === null) {
+        alert(`Canceled.`);
+      } else if (confirmedPassword === currentPassword && email === `user@gmail.com`
+            || confirmedPassword === currentPassword && email === `admin@gmail.com`) {
+        let newPassword = prompt(`Please, enter your new password:`, ``);
+        if (newPassword === `` || newPassword === null) {
+          alert(`Canceled.`);
+        } else if (newPassword.length < PASSWORD_MIN_LENGTH) {
+          alert(`It’s too short password. Sorry.`);
+        } else {
+          confirmedPassword = prompt(`Please, enter your new password again:`, ``);
+          if (confirmedPassword === `` || confirmedPassword === null) {
+            alert(`Canceled.`);
+          } else {
+            alert(confirmedPassword === newPassword
+              ? `You have successfully changed your password.`
+              : `You wrote the wrong password.`);
+          }
+        }
+      } else {
+        alert(`Wrong password`);
+      }
+    } else {
+      alert(`You have failed the change.`);
+    }
+  } else {
+    alert(`Wrong password`);
+  }
+} else {
+  alert(`I don’t know you`);
+}
--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,1 +1,43 @@
-// Your code goes here
+const MAX_ATTEMPTS = 3;
+const RANGE_STEP = 4;
+const PRIZE_STEP = 2;
+const INITIAL_MAX_PRIZE = 100;
+const INITIAL_MAX_RANGE_NUMBER = 8;
+const MIN_RANGE_NUMBER = 0;
+
+let isPlaying = confirm(`Do you want to play a game?`);
+
+while (isPlaying) {
+  let maxRangeNumber = INITIAL_MAX_RANGE_NUMBER;
+  let numberToGuess = Math.floor(Math.random() * (maxRangeNumber - MIN_RANGE_NUMBER + 1)) + MIN_RANGE_NUMBER;
+  let maxPrize = INITIAL_MAX_PRIZE;
+  let currentPrize = maxPrize;
+  let totalPrize = 0;
+  let attemptsLeft = MAX_ATTEMPTS;
+  let isContinue = true;
+  while (attemptsLeft && isContinue) {
+    let playerAnswer = parseFloat(prompt(`Choose a roulette pocket number from ${MIN_RANGE_NUMBER} to ${maxRangeNumber}
+Attempts left: ${attemptsLeft}
+Total prize: ${totalPrize}$
+Possible prize on current attempt: ${currentPrize}$
+`, ``));
+    if (playerAnswer === numberToGuess) {
+      totalPrize += currentPrize;
+      isContinue = confirm(`Congratulation, you won!   Your prize is: ${totalPrize} $. Do you want to continue?`);
+      if (isContinue) {
+        maxRangeNumber += RANGE_STEP;
+        numberToGuess = Math.floor(Math.random() * (maxRangeNumber - MIN_RANGE_NUMBER + 1)) + MIN_RANGE_NUMBER;
+        maxPrize *= PRIZE_STEP;
+        currentPrize = maxPrize;
+        attemptsLeft = MAX_ATTEMPTS;
+      }
+    } else {
+      currentPrize /= PRIZE_STEP;
+      attemptsLeft--;
+    }
+  }
+  totalPrize = attemptsLeft === 0 ? 0 : totalPrize;
+  alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);
+  isPlaying = confirm(`Do you want to play again?`);
+}
+alert(`You did not become a billionaire, but can.`);

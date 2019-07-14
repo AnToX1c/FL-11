89a88efc57cc5afa877924c3c52ef108612c9ca7
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-07-14 23:49:45 +0300

    :nine: Done Data types HW

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,1 +1,85 @@
-// Your code goes here
+const getNumbers = str => {
+  let result = [];
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(Number(arr[i]))) {
+      result.push(Number(arr[i]));
+    }
+  }
+  return result;
+};
+
+const findTypes = (...args) => {
+  let result = {};
+  for (let i = 0; i < args.length; i++) {
+    let elemType = typeof args[i];
+    result[elemType] = result.hasOwnProperty(elemType) ? result[elemType] + 1 : 1;
+  }
+  return result;
+};
+
+const executeforEach = (arr, func) => {
+  for (let i = 0; i < arr.length; i++) {
+    func(arr[i]);
+  }
+};
+
+const mapArray = (arr, func) => {
+  let result = [];
+  executeforEach(arr, function(element){
+      result.push(func(element));
+    });
+  return result;
+};
+
+const filterArray = (arr, func) => {
+  let result = [];
+  executeforEach(arr, function(element){
+    if (func(element)) {
+      result.push(element);
+    }
+  });
+  return result;
+};
+
+const showFormattedDate = (inputDate) => {
+  const month = inputDate.toLocaleString('default', {month: 'short'});
+  return `Date: ${month} ${inputDate.getDate()} ${inputDate.getFullYear()}`;
+};
+
+const canConvertToDate = (str) => !isNaN(new Date(str));
+
+const daysBetween = (date1, date2) => {
+  const MS_PER_DAY = 86400000;
+  const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+  const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+  return Math.floor(Math.abs(utc2 - utc1) / MS_PER_DAY);
+};
+
+const getAmountOfAdultPeople = (data) => {
+  const DAYS_FOR_ADULT = 6570;
+  const today = new Date();
+  return filterArray(data, function(el) {
+    return daysBetween(today, new Date(el.birthday)) > DAYS_FOR_ADULT;
+  }).length;
+};
+
+const keys = (data) => {
+  let result = [];
+  for (let key in data) {
+    if ({}.hasOwnProperty.call(data, key)) {
+      result.push(key);
+    }
+  }
+  return result;
+};
+
+const values = (data) => {
+  let result = [];
+  for (let key in data) {
+    if ({}.hasOwnProperty.call(data, key)) {
+      result.push(data[key]);
+    }
+  }
+  return result;
+};
